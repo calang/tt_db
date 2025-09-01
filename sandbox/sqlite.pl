79dd5baa837ab46c92409acf2d5c74e1b449f309
Carlos A. Lang-Sanou <carlos.lang@gmail.com>
2025-09-01 14:13:51 -0600

    create examples of using sqlite with swiprolog

---
+++ sandbox/sqlite.pl
@@ -0,0 +1,33 @@
+% experiments using sqlite package
+
+/*
+to install the package:
+?- pack_install(prosqlite).
+
+to verify installation:
+?- pack_list_installed.
+*/
+
+:- use_module(library(prosqlite)).
+
+% create empty the sqlite DB file, if needed
+:- shell('> uniprot.sqlite').
+
+:- sqlite_connect('uniprot.sqlite', uniprot).
+
+:- sqlite_query('CREATE TABLE movie(title, year, score)', Row).
+
+:- sqlite_query('\c
+    INSERT INTO movie VALUES \c
+        ("Monty Python and the Holy Grail", 1975, 8.2), \c
+        ("And Now for Something Completely Different", 1971, 7.5) \c
+    ',
+        Row
+).
+
+q(Row) :- sqlite_query('select * from movie', Row).
+
+% end of the script
+end :- sqlite_disconnect(uniprot).
+
+
